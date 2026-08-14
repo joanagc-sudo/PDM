142d81445452130e4c8be9db2fa458db3a478a0d
joanagc-sudo <joanagc.goncalves@gmail.com>
2026-08-14 12:13:03 -0300

    contador

--- app/src/main/java/com/example/aplicativo/MainActivity.java
+++ app/src/main/java/com/example/aplicativo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.aplicativo;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +11,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+int contador = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        Button b=findViewById(R.id.button);
+        TextView tv = findViewById(R.id.textView);
+
+        b.setOnClickListener(view -> {
+            contador++;
+            tv.setText(Integer.toString(contador));
+        });
+
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
