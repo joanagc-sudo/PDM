6faa41f8c2d57f65589921623a5d85c65b4f7522
joanagc-sudo <joanagc.goncalves@gmail>
2026-09-25 11:59:07 -0300

    botão imagens

--- app/src/main/java/com/example/appdesergio/MainActivity.java
+++ app/src/main/java/com/example/appdesergio/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.appdesergio;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 
 import androidx.activity.EdgeToEdge;
@@ -13,25 +12,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.aplicativo.R;
-
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
+
+
+    int posicao = 0;
+    Button botaoAvancar;
+    Button botaoVoltar;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        botaoVoltar = findViewById(R.id.btnAnterior);
+        botaoAvancar = findViewById(R.id.btnProximo);
+        imageView = findViewById(R.id.imageView);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Integer[] imagens = new Integer[]{
+        Integer[] images = new Integer[]{
                 R.drawable.cachorro,
                 R.drawable.gardem,
                 R.drawable.happy,
@@ -39,12 +41,19 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.porquinho
         };
 
-        Button botaoVoltar, botaoAvancar;
-        botaoVoltar = findViewById(R.id.btnAnterior);
-        botaoAvancar =  findViewById(R.id.btnProximo);
+        botaoAvancar.setOnClickListener( v -> {
+            if(posicao== images.length-1) posicao = 0;
+            posicao++;
+            imageView.setImageResource(images[posicao]);
 
-        botaoAvancar.setOnClickListener(View v ->{
-            imageView.setImageResource(imagens[posicao]);
+        });
+
+        botaoVoltar.setOnClickListener( v -> {
+
+            posicao--;
+            if(posicao < 0) posicao = images.length-1;
+            imageView.setImageResource(images[posicao]);
+
         });
     }
 }
