92b9e3cb52ec605dd0804dab0d9af58d56f061ab
joanagc-sudo <joanagc.goncalves@gmail.com>
2026-09-26 13:59:10 -0300

    correção do layout

--- app/src/main/java/com/example/appdesergio/Calcular.java
+++ app/src/main/java/com/example/appdesergio/Calcular.java
@@ -34,7 +34,8 @@ public class Calcular extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_calcular);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main),
+                (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
